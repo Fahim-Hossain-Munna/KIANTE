e25d7c6aadbdb5d554233226512de7d2c3beffc7
Fahim Hossain Munna <87969487+Fahim-Hossain-Munna@users.noreply.github.com>
2024-03-22 09:35:02 +0600

    hello virtual people

---
+++ kiante/assets/js/main.js
@@ -0,0 +1,195 @@
+; (function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+
+        /**-----------------------------
+         *  Navbar fix
+         * ---------------------------*/
+        $(document).on('click', '.navbar-area .navbar-nav li.menu-item-has-children>a', function (e) {
+            e.preventDefault();
+        })
+       
+        /*-------------------------------------
+            menu
+        -------------------------------------*/
+        $('.navbar-area .menu').on('click', function() {
+            $(this).toggleClass('open');
+            $('.navbar-area .navbar-collapse').toggleClass('sopen');
+        });
+    
+        // mobile menu
+        if ($(window).width() < 992) {
+            $(".in-mobile").clone().appendTo(".sidebar-inner");
+            $(".in-mobile ul li.menu-item-has-children").append('<i class="fas fa-chevron-right"></i>');
+            $('<i class="fas fa-chevron-right"></i>').insertAfter("");
+
+            $(".menu-item-has-children a").on('click', function(e) {
+                // e.preventDefault();
+
+                $(this).siblings('.sub-menu').animate({
+                    height: "toggle"
+                }, 300);
+            });
+        }
+
+        var menutoggle = $('.menu-toggle');
+        var mainmenu = $('.navbar-nav');
+        
+        menutoggle.on('click', function() {
+            if (menutoggle.hasClass('is-active')) {
+                mainmenu.removeClass('menu-open');
+            } else {
+                mainmenu.addClass('menu-open');
+            }
+        });
+
+        /*--------------------------------------------
+            Search Popup
+        ---------------------------------------------*/
+        var bodyOvrelay =  $('#body-overlay');
+        var searchPopup = $('#search-popup');
+
+        $(document).on('click','#body-overlay',function(e){
+            e.preventDefault();
+        bodyOvrelay.removeClass('active');
+            searchPopup.removeClass('active');
+        });
+        $(document).on('click','.search',function(e){
+            e.preventDefault();
+            searchPopup.addClass('active');
+        bodyOvrelay.addClass('active');
+        });
+
+        /*--------------------------------------------------
+            select onput
+        ---------------------------------------------------*/
+        $(document).ready(function() {
+            $('select').niceSelect();
+        });
+
+
+        /* -----------------------------------------------------
+            Variables
+        ----------------------------------------------------- */
+        var leftArrow = '<i class="fa fa-angle-left"></i>';
+        var rightArrow = '<i class="fa fa-angle-right"></i>';
+
+        /* -------------------------------------------------
+            Magnific JS 
+        ------------------------------------------------- */
+        $('.video-play-btn').magnificPopup({
+          type: 'iframe',
+          removalDelay: 260,
+          mainClass: 'mfp-zoom-in',
+        });
+        $.extend(true, $.magnificPopup.defaults, {
+          iframe: {
+            patterns: {
+              youtube: {
+                index: 'youtube.com/', 
+                id: 'v=', 
+                src: 'https://www.youtube.com/embed/Wimkqo8gDZ0' 
+              }
+            }
+          }
+        });
+
+        /*------------------------------------------------
+            banner-slider
+        ------------------------------------------------*/
+        $('.post-banner-slider').owlCarousel({
+            loop: true,
+            margin: 20,
+            nav: false,
+            dots: true,
+            smartSpeed:1500,
+            center: true,
+            items: 2,
+            autoWidth:true,
+            navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],           
+        });
+
+        /*------------------------------------------------
+            banner-slider
+        ------------------------------------------------*/
+        $('.post-grid-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: false,
+            dots: false,
+            smartSpeed:1500,
+            animateOut: 'fadeOut',
+            autoplay:true,
+            autoplayTimeout:2000,
+            items: 1,
+            navText: ['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],           
+        });
+        
+        
+
+        /*-------------------------------------------------
+            wow js init
+        --------------------------------------------------*/
+        new WOW().init();
+
+        /*------------------
+           back to top
+        ------------------*/
+        $(document).on('click', '.back-to-top', function () {
+            $("html,body").animate({
+                scrollTop: 0
+            }, 2000);
+        });
+
+    });
+
+    $(window).on("scroll", function() {
+        /*---------------------------------------
+        sticky menu activation && Sticky Icon Bar
+        -----------------------------------------*/
+
+        var mainMenuTop = $(".navbar-area");
+        if ($(window).scrollTop() >= 1) {
+            mainMenuTop.addClass('navbar-area-fixed');
+        }
+        else {
+            mainMenuTop.removeClass('navbar-area-fixed');
+        }
+        
+        var ScrollTop = $('.back-to-top');
+        if ($(window).scrollTop() > 1000) {
+            ScrollTop.fadeIn(1000);
+        } else {
+            ScrollTop.fadeOut(1000);
+        }
+    });
+
+
+    $(window).on('load', function () {
+
+        /*-----------------
+            preloader
+        ------------------*/
+        var preLoder = $("#preloader");
+        preLoder.fadeOut(0);
+
+        /*-----------------
+            back to top
+        ------------------*/
+        var backtoTop = $('.back-to-top')
+        backtoTop.fadeOut();
+
+        /*---------------------
+            Cancel Preloader
+        ----------------------*/
+        $(document).on('click', '.cancel-preloader a', function (e) {
+            e.preventDefault();
+            $("#preloader").fadeOut(2000);
+        });
+
+    });
+
+
+
+})(jQuery);
